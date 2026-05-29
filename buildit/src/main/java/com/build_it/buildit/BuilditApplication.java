4368432fdb9d8839e8a42e0d5db92ec2b1c3a70b
Felix Pelletier <felix.pelletierf@hotmail.com>
2026-05-29 14:09:49 -0400

    initial commit

---
+++ buildit/src/main/java/com/build_it/buildit/BuilditApplication.java
@@ -0,0 +1,13 @@
+package com.build_it.buildit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuilditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuilditApplication.class, args);
+	}
+
+}
---
+++ buildit/src/test/java/com/build_it/buildit/BuilditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.build_it.buildit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuilditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
